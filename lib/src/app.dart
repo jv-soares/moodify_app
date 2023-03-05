22f12b613d2178262cf7e35e84215057dc27edbc
João Vitor <j.soares@aparkado.de>
2023-03-05 19:16:57 -0300

    configure theme colors

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
+import 'theme/color_schemes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Moodify',
-      home: HomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
