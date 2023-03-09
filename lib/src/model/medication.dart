528c6e0374ff7605f1d3cfa8953ef0be2c60199d
João Vitor <j.soares@aparkado.de>
2023-03-08 23:42:18 -0300

    add equatable

--- lib/src/model/medication.dart
+++ lib/src/model/medication.dart
@@ -1,20 +1,42 @@
-class Medication {
+import 'package:equatable/equatable.dart';
+
+class Medication extends Equatable {
   final String id;
   final String name;
   final int tabletsTaken;
   final Dose dose;
 
-  Medication({
+  const Medication({
     required this.id,
     required this.name,
     required this.tabletsTaken,
     required this.dose,
   });
+
+  Medication copyWith({
+    String? id,
+    String? name,
+    int? tabletsTaken,
+    Dose? dose,
+  }) {
+    return Medication(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      tabletsTaken: tabletsTaken ?? this.tabletsTaken,
+      dose: dose ?? this.dose,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, name, tabletsTaken, dose];
 }
 
-class Dose {
+class Dose extends Equatable {
   final double value;
   final String unitOfMeasurement;
 
-  Dose(this.value, this.unitOfMeasurement);
+  const Dose(this.value, this.unitOfMeasurement);
+
+  @override
+  List<Object?> get props => [value, unitOfMeasurement];
 }
