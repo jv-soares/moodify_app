91cb17c768fa684cef449e52a7f9086e43d4f9c7
João Vitor <j.soares@aparkado.de>
2023-03-09 22:08:28 -0300

    use MirroredIntegersGenerator on LifeEventForm

--- lib/src/pages/daily_entry_form/components/life_event_form.dart
+++ lib/src/pages/daily_entry_form/components/life_event_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moodify_app/src/pages/daily_entry_form/components/life_event_section.dart';
+import 'package:moodify_app/src/utils/mirrored_integers_generator.dart';
 import 'package:provider/provider.dart';
 
 import '../../../widgets/moodify_text_field.dart';
@@ -20,17 +21,10 @@ class LifeEventForm extends StatelessWidget {
         SegmentedButton(
           showSelectedIcon: false,
           onSelectionChanged: (p0) => notifier.onImpactRatingChanged(p0.single),
-          segments: [
-            ButtonSegment(value: -4, label: Text('-4')),
-            ButtonSegment(value: -3, label: Text('-3')),
-            ButtonSegment(value: -2, label: Text('-2')),
-            ButtonSegment(value: -1, label: Text('-1')),
-            ButtonSegment(value: 0, label: Text('0')),
-            ButtonSegment(value: 1, label: Text('+1')),
-            ButtonSegment(value: 2, label: Text('+2')),
-            ButtonSegment(value: 3, label: Text('+3')),
-            ButtonSegment(value: 4, label: Text('+4')),
-          ],
+          segments: MirroredIntegersGenerator.generate(length: 9).map((e) {
+            final valueString = e.isNegative ? '$e' : '+$e';
+            return ButtonSegment(value: e, label: Text(valueString));
+          }).toList(),
           selected: {notifier.value.impactRating},
         ),
       ],
