8727a7e170479a41d5b3a427dc2e5a1f6f3a7f3e
João Vitor <j.soares@aparkado.de>
2023-03-09 22:17:02 -0300

    remove plus sign from 0

--- lib/src/pages/daily_entry_form/components/life_event_form.dart
+++ lib/src/pages/daily_entry_form/components/life_event_form.dart
@@ -22,12 +22,21 @@ class LifeEventForm extends StatelessWidget {
           showSelectedIcon: false,
           onSelectionChanged: (p0) => notifier.onImpactRatingChanged(p0.single),
           segments: MirroredIntegersGenerator.generate(length: 9).map((e) {
-            final valueString = e.isNegative ? '$e' : '+$e';
-            return ButtonSegment(value: e, label: Text(valueString));
+            return ButtonSegment(value: e, label: Text(_toFormattedString(e)));
           }).toList(),
           selected: {notifier.value.impactRating},
         ),
       ],
     );
+  }
+
+  String _toFormattedString(int value) {
+    if (value.isNegative) {
+      return '$value';
+    } else if (value == 0) {
+      return '$value';
+    } else {
+      return '+$value';
+    }
   }
 }
