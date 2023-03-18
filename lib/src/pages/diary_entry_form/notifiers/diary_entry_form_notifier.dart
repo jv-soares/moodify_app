151ebe0692e6f05e1c5a7c8a014852f24f28e2ce
João Vitor <j.soares@aparkado.de>
2023-03-18 17:20:34 -0300

    add repository delay

--- lib/src/pages/diary_entry_form/notifiers/diary_entry_form_notifier.dart
+++ lib/src/pages/diary_entry_form/notifiers/diary_entry_form_notifier.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../../repositories/diary_entry_repository.dart';
 
+import '../../../repositories/diary_entry_repository.dart';
 import '../view_models/diary_entry_view_model.dart';
 
 part 'diary_entry_form_state.dart';
@@ -12,7 +12,7 @@ class DiaryEntryFormNotifier extends ValueNotifier<DiaryEntryFormState> {
 
   Future<void> save(DiaryEntryViewModel diaryEntry) async {
     value = Loading();
-    _repository.create(diaryEntry.toModel());
+    await _repository.create(diaryEntry.toModel());
     value = Saved();
   }
 }
--- lib/src/repositories/diary_entry_repository.dart
+++ lib/src/repositories/diary_entry_repository.dart
@@ -12,14 +12,18 @@ class TempDiaryEntryRepository implements DiaryEntryRepository {
   @override
   Future<String> create(DiaryEntry entry) async {
     _entries.add(entry);
+    await _delay;
     return entry.id;
   }
 
   @override
   Future<DiaryEntry> read(String id) async {
+    await _delay;
     return _entries.singleWhere(
       (element) => element.id == id,
       orElse: () => throw DiaryEntryNotFoundFailure(),
     );
   }
+
+  Future<void> get _delay => Future.delayed(const Duration(seconds: 1));
 }
