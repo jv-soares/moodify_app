3057c1861d5c8df95cb9bca5eb080baa4ac0aaf0
João Vitor <j.soares@aparkado.de>
2023-03-18 23:16:30 -0300

    add snap effect to chart

--- lib/src/pages/diary_dashboard/components/episodes_chart_entry.dart
+++ lib/src/pages/diary_dashboard/components/episodes_chart_entry.dart
@@ -1,9 +1,10 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 import '../../../models/episode_severity.dart';
 
-class EpisodesChart extends StatelessWidget {
+class EpisodesChart extends StatefulWidget {
   final List<EpisodesChartEntry> episodes;
   final double rowHeight;
 
@@ -14,21 +15,50 @@ class EpisodesChart extends StatelessWidget {
   });
 
   @override
+  State<EpisodesChart> createState() => _EpisodesChartState();
+}
+
+class _EpisodesChartState extends State<EpisodesChart> {
+  late List<EpisodesChartEntry> _sortedEpisodes;
+  late EpisodesChartEntry _selectedEpisode;
+
+  @override
+  void initState() {
+    super.initState();
+    _sortedEpisodes = _getSortedEpisodes();
+    _selectedEpisode = _sortedEpisodes.first;
+  }
+
+  @override
+  void didUpdateWidget(covariant EpisodesChart oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _sortedEpisodes = _getSortedEpisodes();
+  }
+
+  List<EpisodesChartEntry> _getSortedEpisodes() {
+    return widget.episodes.sortedByCompare(
+      (e) => e.createdAt,
+      (a, b) => b.compareTo(a),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
+      height: widget.rowHeight * 9,
       color: Colors.green.shade100,
       child: Stack(
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(vertical: rowHeight / 2),
+            padding: EdgeInsets.symmetric(vertical: widget.rowHeight / 2),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: List.generate(
                 4,
                 (index) => Container(
-                  margin: EdgeInsets.symmetric(vertical: rowHeight / 2),
+                  margin: EdgeInsets.symmetric(vertical: widget.rowHeight / 2),
                   color: Theme.of(context).colorScheme.surfaceVariant,
-                  height: rowHeight,
+                  height: widget.rowHeight,
                   width: double.infinity,
                 ),
               ),
@@ -37,7 +67,6 @@ class EpisodesChart extends StatelessWidget {
           Align(
             alignment: Alignment.centerRight,
             child: Container(
-              height: rowHeight * (episodes.length - 1),
               width: 32,
               margin: const EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
@@ -47,45 +76,44 @@ class EpisodesChart extends StatelessWidget {
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(right: 8),
-            child: SingleChildScrollView(
+            margin: const EdgeInsets.only(right: 16),
+            child: ScrollSnapList(
+              listViewPadding: EdgeInsets.zero,
+              onItemFocus: (index) {
+                setState(() => _selectedEpisode = _sortedEpisodes[index]);
+              },
               scrollDirection: Axis.horizontal,
+              itemCount: widget.episodes.length,
+              itemSize: 32,
               reverse: true,
-              child: Row(
-                textDirection: TextDirection.rtl,
-                children: episodes
-                    .sortedByCompare(
-                        (e) => e.createdAt, (a, b) => b.compareTo(a))
-                    .map((e) {
-                  return Container(
-                    height: rowHeight * (episodes.length - 1),
-                    margin: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        _buildMarkerFrame(
-                          child: Container(
-                            height: 16,
-                            width: 16,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
+              itemBuilder: (context, index) {
+                final item = _sortedEpisodes[index];
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    _buildMarkerFrame(
+                      child: Container(
+                        height: 16,
+                        width: 16,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _selectedEpisode == item
+                              ? Colors.white
+                              : Theme.of(context).colorScheme.primary,
                         ),
-                        SizedBox(height: _calculateMarkerPosition(e.episode)),
-                        SizedBox(height: rowHeight),
-                        _buildMarkerFrame(
-                          child: Text(
-                            e.createdAt.day.toString(),
-                            style: Theme.of(context).textTheme.labelMedium,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  );
-                }).toList(),
-              ),
+                    SizedBox(height: _calculateMarkerPosition(item.episode)),
+                    SizedBox(height: widget.rowHeight),
+                    _buildMarkerFrame(
+                      child: Text(
+                        item.createdAt.day.toString(),
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ],
@@ -94,7 +122,7 @@ class EpisodesChart extends StatelessWidget {
   }
 
   Widget _buildMarkerFrame({required Widget child}) {
-    final verticalMargin = (rowHeight - 16) / 2;
+    final verticalMargin = (widget.rowHeight - 16) / 2;
     return Container(
       margin: EdgeInsets.symmetric(vertical: verticalMargin, horizontal: 8),
       child: child,
@@ -103,9 +131,9 @@ class EpisodesChart extends StatelessWidget {
 
   double _calculateMarkerPosition(EpisodeSeverity episode) {
     if (episode is Mania) {
-      return episode.level.value * rowHeight;
+      return episode.level.value * widget.rowHeight;
     } else if (episode is Depression) {
-      return episode.level.value * rowHeight;
+      return episode.level.value * widget.rowHeight;
     }
     return 0;
   }
