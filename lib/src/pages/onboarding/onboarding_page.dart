425d8a3dc9ff499e0af277d9acd892301c1d5694
João Vitor <j.soares@aparkado.de>
2023-05-29 12:22:55 -0300

    add page indicator and page transition

--- lib/src/pages/onboarding/onboarding_page.dart
+++ lib/src/pages/onboarding/onboarding_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../widgets/moodify_button.dart';
 
@@ -19,47 +20,72 @@ class _OnboardingPageState extends State<OnboardingPage> {
     _pageController = PageController();
   }
 
+  void _finishOnboarding() {
+    SharedPreferences.getInstance().then(
+      (instance) => instance.setBool('didOnboard', true),
+    );
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView(
-              controller: _pageController,
-              children: _sections
-                  .map((e) => _OnboardingView(
-                        image: SvgPicture.asset(e.assetPath),
-                        title: e.title,
-                        description: e.description,
-                      ))
-                  .toList(),
-            ),
-          ),
-          const SizedBox(height: 64),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(
-              _sections.length,
-              (index) => Container(
-                margin: const EdgeInsets.symmetric(horizontal: 8),
-                width: 24,
-                height: 8,
-                color: Colors.red,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(automaticallyImplyLeading: false),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                controller: _pageController,
+                children: _sections
+                    .map((e) => _OnboardingView(
+                          image: SvgPicture.asset(e.assetPath),
+                          title: e.title,
+                          description: e.description,
+                        ))
+                    .toList(),
               ),
             ),
-          ),
-          const SizedBox(height: 32),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
-            child: MoodifyFilledButton(
-              label: 'Próximo',
-              horizontallyExpanded: true,
-              onPressed: () {},
+            const SizedBox(height: 64),
+            AnimatedBuilder(
+              animation: _pageController,
+              builder: (context, _) => Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  _sections.length,
+                  (index) => _OnboardingIndicator(
+                    index: index,
+                    pageController: _pageController,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+              child: AnimatedBuilder(
+                  animation: _pageController,
+                  builder: (context, _) {
+                    final currentPage = _pageController.page ?? 0;
+                    return MoodifyFilledButton(
+                      label: currentPage > 1.4 ? 'Começar' : 'Próximo',
+                      horizontallyExpanded: true,
+                      onPressed: () {
+                        if (currentPage > 1) {
+                          _finishOnboarding();
+                        } else {
+                          _pageController.nextPage(
+                            curve: Curves.easeInOut,
+                            duration: const Duration(milliseconds: 250),
+                          );
+                        }
+                      },
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -68,6 +94,58 @@ class _OnboardingPageState extends State<OnboardingPage> {
   void dispose() {
     _pageController.dispose();
     super.dispose();
+  }
+}
+
+class _OnboardingIndicator extends StatefulWidget {
+  final int index;
+  final PageController pageController;
+
+  const _OnboardingIndicator({
+    required this.index,
+    required this.pageController,
+  });
+
+  @override
+  State<_OnboardingIndicator> createState() => _OnboardingIndicatorState();
+}
+
+class _OnboardingIndicatorState extends State<_OnboardingIndicator> {
+  late final ColorTween _tween;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final colorScheme = Theme.of(context).colorScheme;
+    _tween = ColorTween(
+      begin: colorScheme.outlineVariant,
+      end: colorScheme.primary,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      width: 16,
+      height: 4,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: _tween.transform(_calculateIndicatorAnimationValue()),
+      ),
+    );
+  }
+
+  double _calculateIndicatorAnimationValue() {
+    final value = widget.pageController.page ?? 0;
+    if (widget.index == 0 && value < 1) {
+      return 1 - value;
+    } else if (value <= widget.index && value > widget.index - 1) {
+      return value - (widget.index - 1);
+    } else if (value > widget.index && value < widget.index + 1) {
+      return (widget.index + 1) - value;
+    }
+    return 0;
   }
 }
 
--- lib/src/pages/splash/splash_page.dart
+++ lib/src/pages/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moodify_app/src/app.dart';
+import 'package:moodify_app/src/pages/onboarding/onboarding_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
@@ -27,11 +28,50 @@ class _SplashPageState extends State<SplashPage> {
   }
 
   void _pushOnboarding() {
-    Navigator.of(context).pushReplacementNamed(AppRoutes.onboarding);
+    Navigator.of(context).pushReplacementNamed(AppRoutes.home);
+    Navigator.of(context).push(_OnboardingPageRoute());
   }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  }
+}
+
+class _OnboardingPageRoute extends PageRoute {
+  @override
+  Color? get barrierColor => null;
+
+  @override
+  String? get barrierLabel => null;
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 450);
+
+  @override
+  Widget buildPage(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+  ) {
+    return const OnboardingPage();
+  }
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return SlideTransition(
+      position: Tween(begin: const Offset(0, -1), end: Offset.zero)
+          .chain(CurveTween(curve: Curves.easeOutCubic))
+          .animate(animation),
+      child: child,
+    );
   }
 }
