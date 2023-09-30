e86f3c0c42153a43cb3e1c965dc8c969f8b68ba0
João Vitor <j.soares@aparkado.de>
2023-09-30 15:22:01 +0200

    dont setup crashlytics on debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -14,7 +13,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'pt_BR';
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  _setupCrashlytics();
+  if (!kDebugMode) _setupCrashlytics();
   await initializeDateFormatting();
   await AppContainer.initialize();
   runApp(const App());
